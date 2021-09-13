dfbf4e8f23e903312fd2a86b55ec2977b79a389e
Leonardo Silva <71605225+leosmsilvx@users.noreply.github.com>
2021-09-13 13:35:56 -0300

    Update botpiano.py

--- botpiano.py
+++ botpiano.py
@@ -1,6 +1,6 @@
 # &/xZ
 # Site: https://m.joguix.com/jogar/piano-tiles-2-online/
-# Personal Record: 1962
+# Personal Record: 2435
 import pyautogui
 import time
 import keyboard
@@ -11,26 +11,24 @@ def click():
         if pyautogui.pixel(rec_px[i], 600)[0] == 0:
             win32api.SetCursorPos((rec_px[i], 600)) 
             win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-            time.sleep(0.01) # Time is necessary cause it "confirm" the click
+            time.sleep(0.001) # Time is necessary cause it confirm the "click"
             win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
 i = 0
 # Pixels on the screen
 rec_px = [760, 920, 1050, 1200]
 while i == 0:
-    # s is the key to start
+    # s is the ket to start
     if keyboard.is_pressed('s') == True:
         close = False
         while close == False:
             try:
                 click()
-            # If it cant click, try again
+            # if catch this error, ignore
             except OSError:
-                click()
+                pass
 
             # q is the key to close
             if keyboard.is_pressed('q') == True:
                 close = True
                 i = 1
-                                       
-
